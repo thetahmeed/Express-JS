2d2a9214068b69b7b8f61a622cc7c5960cf302fb
thetahmeed <tahmeed.com.bd@gmail.com>
2020-12-17 22:10:05 +0600

    routing by E.JS

--- firstapp/README.txt
+++ firstapp/README.txt
@@ -1,3 +1,8 @@
 // create a new package by "npm init --y"
 // install Express JS by "npm install express --save"
 // follow the index.js for the rest
+
+
+// www.mywebsite.com/about
+// ^---------------^ ^----^
+//      url             uri
--- firstapp/index.js
+++ firstapp/index.js
@@ -8,11 +8,24 @@ var mExpress = require('express');
 
 var app = mExpress();
 
+// 1. returning a response
 app.get("/", function(req, res){
 
     res.send("<h1>Welcome to Express JS</h1>");
 
 });
+
+// 2. Routing. // multiplle response acording to different request
+app.get("/chat", function(req, res){
+    res.send("<h1>Chat</h1>");
+});
+app.get("/contact", function(req, res){
+    res.send("<h1>Contact</h1>");
+});
+app.get("/profile", function(req, res){
+    res.send("<h1>Profile</h1>");
+});
+
 
 app.listen(8000, function(){
 
