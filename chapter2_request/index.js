b9ea36ef80268240f617a6a40df6cbe887c57957
thetahmeed <tahmeed.com.bd@gmail.com>
2020-12-18 18:43:16 +0600

    get request from header

--- chapter2_request/index.js
+++ chapter2_request/index.js
@@ -8,11 +8,23 @@ var mExpress = require('express');
 
 var app = mExpress();
 
-// 1. request URL query // ULR: http://localhost:8000?firstName=Tahmeedul&lastName=Islam
-app.get("/", function(req, res){
+// 1. GET request URL query
+// here we are sending request by a URL and getting the request by "req" param.
+
+app.get("//url?firstName=Tahmeedul&lastName=Islam", function(req, res){
 
     let firstName = req.query.firstName
     let lastName = req.query.lastName
+    
+    res.send(firstName+" "+lastName)
+
+})
+
+// 2. GET request header
+app.get("/header", function(req, res){
+
+    let firstName = req.header("firstName")
+    let lastName = req.header("lastName")
     
     res.send(firstName+" "+lastName)
 
