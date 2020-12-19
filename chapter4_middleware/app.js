5aa6ac76526a97708dc7d48ea2625d8c680da267
thetahmeed <tahmeed.com.bd@gmail.com>
2020-12-19 18:15:03 +0600

    self-made middleware and use

--- chapter4_middleware/app.js
+++ chapter4_middleware/app.js
@@ -5,20 +5,37 @@
 
 
 const mExpress = require('express')
-const mMorgan = require('morgan')                      // 1.
+const mMorgan = require('morgan')                       // 1.
 
 
 const mApp = mExpress()
 
-//mApp.use(mMorgan('dev'))                             // 2.
+//mApp.use(mMorgan('dev'))                              // 2. here for all route
 
 mApp.get("/",(req, res) => {
     res.send("Home page")
 })
 
-mApp.get("/about", mMorgan('dev'),(req, res) => {
+mApp.get("/about", mMorgan('dev'),(req, res) => {       // 3. here for a particular route. use comma for multiple
     res.send("About page")
 })
+
+
+// 4. creating a self-made middleware
+function mMiddleware(req, res, next){                   // you have to send these 3 param
+    if(req.url == "/middleware"){
+        res.send('alert("This page blocked by the Admin")')
+    }
+
+    next()                                              // you have to call "next()" at the end
+}
+
+// 4.1 using our mMiddleware
+mApp.get("/middleware", mMiddleware,(req, res) => {
+    res.send("Middlewaare page")
+})
+
+
 
 mApp.listen(8080, () => {
     console.log('I am on PORT: 8080')
