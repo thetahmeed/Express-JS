4b7c6a7c3062d339cbe9d877c6841b72756c2bbc
thetahmeed <tahmeed.com.bd@gmail.com>
2020-12-22 15:51:55 +0600

    installed mogoes

---
+++ chapter6_database/mongoDB/README.txt
@@ -0,0 +1,8 @@
+
+
+
+
+
+
+// here we will use 'mongose' to make relation and manupulate the database
+// command: npm install mongoose --save
--- chapter6_database/mongoDB/app.js
+++ chapter6_database/mongoDB/app.js
@@ -4,3 +4,27 @@
 
 
 
+const mExpress = require('express')
+const mMorgan = require('morgan')
+
+
+
+const mApp = mExpress()
+
+mApp.use(mMorgan('dev'))
+mApp.use(mExpress.urlencoded({extended: true}))
+mApp.use(mExpress.json())
+
+
+
+mApp.get('/', (req, res) => {
+    res.json({
+        message: 'Hello'
+    })
+})
+
+
+const PORT = process.env.PORT || 8080
+mApp.listen(PORT, () => {
+    console.log(`I am on Port: ${PORT}`)
+})
