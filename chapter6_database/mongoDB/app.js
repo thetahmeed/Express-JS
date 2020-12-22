c41024d3f58ecd36fdcedd0dcc3cb41c59724ee0
thetahmeed <tahmeed.com.bd@gmail.com>
2020-12-22 16:29:07 +0600

    put data to mongodb

--- chapter6_database/mongoDB/app.js
+++ chapter6_database/mongoDB/app.js
@@ -6,7 +6,9 @@
 
 const mExpress = require('express')
 const mMorgan = require('morgan')
-const mMongoose = require('mongoose')                       // 01
+
+// 01 import
+const mMongoose = require('mongoose')                      
 
 
 
@@ -17,19 +19,42 @@ mApp.use(mExpress.urlencoded({extended: true}))
 mApp.use(mExpress.json())
 
 
-
-mApp.get('/', (req, res) => {
-    res.json({
-        message: 'Hello Tahmeed'
-    })
+// 03. create a schema // schema defines the shape of model
+let mSchema = mMongoose.Schema
+let testSchema = new mSchema({
+    name: String
 })
 
+// create a model with the help of this schema // first letter should be capital // 1st param will be the table name, first letter will be small and name will be plural
+let Model = mMongoose.model('Model', testSchema)
+
+
+
+mApp.get('/', (req, res) => {
+    
+    // 3.1 putting data to the database acording the shape of our model
+    let model = new Model({
+        name : 'Tahmeed'
+    })
+    // 3.2 saving the data
+    model.save()
+        .then(t => {
+            res.json(t)
+        })
+        .catch(e => {
+            console.log(e)
+            res.status(500).json({
+                error: 'Error occurred'
+            })
+        })
+
+})
 
 const PORT = process.env.PORT || 8080
 
-// 02
+// 02 connect
 mMongoose
-    .connect(`mongodb+srv://tahmeedul:cK4H6VVF9fPQPYF6@cluster0.mvhfs.mongodb.net/<dbname>?retryWrites=true&w=majority`, {
+    .connect(`mongodb+srv://tahmeedul:cK4H6VVF9fPQPYF6@cluster0.mvhfs.mongodb.net/user?retryWrites=true&w=majority`, {
         useNewUrlParser: true
     }).then(() => {
         mApp.listen(PORT, () => {
