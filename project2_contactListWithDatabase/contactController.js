7fd57c7fac0a13608fdb09978469b8f05431b636
thetahmeed <tahmeed.com.bd@gmail.com>
2020-12-23 18:08:57 +0600

    delete contact

--- project2_contactListWithDatabase/contactController.js
+++ project2_contactListWithDatabase/contactController.js
@@ -42,8 +42,8 @@ exports.getContactById = (req, res) => {
 
     // Contacts.findOne({_id: id}) or
     Contacts.findById(id)
-        .then(contacts => {
-            res.json(contacts)
+        .then(contact => {
+            res.json(contact)
         })
         .catch(e => {
             console.log(e)
@@ -54,9 +54,37 @@ exports.getContactById = (req, res) => {
 }
 
 exports.updateContactById = (req, res) => {
-    res.send('3')
+    let { name, email, phone } = req.body
+    let { id } = req.params
+
+    Contacts.findOneAndUpdate(
+        { _id: id },                      // where we want to update
+        { $set: { name, email, phone } },   // what we want to update
+        { new: true })                    // to return the new value
+
+        .then(contact => {
+            res.json(contact)
+        })
+        .catch(e => {
+            console.log(e)
+            res.json({
+                error: 'Error!'
+            })
+        })
+
 }
 
 exports.deleteContactById = (req, res) => {
-    res.send('4')
+    let { id } = req.params
+
+    Contacts.findOneAndDelete({_id: id})
+        .then(contact => {
+            res.json(contact)
+        })
+        .catch(e => {
+            console.log(e)
+            res.json({
+                error: 'Error!'
+            })
+        })
 }
